ac3306ba206e64ac5e47d7d1e151a8566b9a593a
Luke Nisbet <luke78910@gmail.com>
2021-06-06 17:27:19 +0100

    added vieport considerations and additional modal styles

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,7 +4,7 @@ const Option = (props) =>
     (
         <div className="option">
             
-                    <p>{props.count}. {props.optionText}</p>
+                    <p className="option__text">{props.count}. {props.optionText}</p>
                     
                     <button className="button button--link" onClick={(e) => {
                         props.handleDeleteOption(props.optionText)
